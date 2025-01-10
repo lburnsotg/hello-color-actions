0e1c1b5e637cdf7399bf93cf161965906f9a9c13
Leon Burns <leon@Leons-MacBook-Air.local>
2025-01-10 18:40:52 -0500

    Third Change

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Nation's Capitol Washington DC!";
+const hello = () => "Hello World from the Office Hours in the terminal!";
 console.log(hello());
 exports.hello = hello;
